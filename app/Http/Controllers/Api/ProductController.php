c867e49a3ee3124a39643a434f1303866894cc2b
jpwebdev <jpwebdeveloper@gmail.com>
2020-06-26 19:21:35 -0300

    API Rest CRUD

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * @var Product
+     */
+    private $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    public function index()
+    {
+       $products = $this->product->all();
+
+       return response()->json($products);
+       //return $products;
+    }
+
+    public function show($id)
+    {
+        $products = $this->product->find($id);
+
+        return response()->json($products);
+        //return $products;
+    }
+
+    public function save(Request $request)
+    {
+        $data = $request->all();
+        $product = $this->product->create($data);
+        return response()->json($product);
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        $product = $this->product->find($data['id']);
+        $product->update($data);
+        return response()->json($product);
+    }
+
+    public function delete($id)
+    {
+        $product = $this->product->find($id);
+        $product->delete();
+
+        return response()->json(['data' => ['msg' =>'Produto foi removido com sucesso!']]);
+    }
+
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/test', function (Request $request) {
+
+    //dd($request->headers->all());
+    //dd($request->headers->get('authorization'));
+
+    $response = new \Illuminate\Http\Response(json_encode(['msg' => 'Minha primeira resposta de API']));
+    $response->header('Content-Type', 'application/json');
+    return $response;
+});
+
+//Products Route
+//Route::get('/products', function(){
+//return \App\Product::all();
+//});
+
+Route::namespace('Api')->prefix('products')->group(function(){
+    Route::get('/', 'ProductController@index');
+    Route::get('/{id}', 'ProductController@show');
+    Route::post('/','ProductController@save');
+    Route::put('/','ProductController@update');
+    Route::patch('/','ProductController@update');
+    Route::delete('/{id}', 'ProductController@delete');
+});
+
+
